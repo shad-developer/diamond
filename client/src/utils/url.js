79529165d0539b57074e56e893fd01102bb18b55
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-02-07 18:31:30 +0500

    url.js

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,2 +1,4 @@
-// export const BACKEND_URL = "https://diamond-app.onrender.com/api";
-export const BACKEND_URL = "http://localhost:5000/api";
+export const BACKEND_URL = "https://diamond-gdwr.onrender.com/api";
+// export const BACKEND_URL = "http://localhost:5000/api";
+
+
