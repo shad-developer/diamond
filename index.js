91dca2b548ec9c7030be4d41c8b321a55010057d
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-03-03 20:09:31 +0500

    index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://diamond-gdwr.onrender.com",
     credentials: true,
   })
 );
